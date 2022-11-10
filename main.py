4c78aaf78c6d62afb98e87ab2dec1dc04be685c9
Giulio Iannello <g.iannello@unicampus.it>
2022-11-10 09:22:35 +0100

    Delineato strategia di soluzione sotto forma di commenti

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 10 09:00:02 2022
+
+@author: iannello
+
+programma che anonimizza una lista di log e salva la corrispondenza
+tra nomi e codici assegnati
+"""
+
+# leggere il file di log (lista di liste di stringhe)
+
+# creare un dizionario vuoto che rappresenta la tabella (codice, nome)
+
+# inizializzare il generatore di codici
+
+# per ogni log procedere all'anonimizzazione
+# se il nome non è nella tabella
+#   generare un nuovo codice e inserire la (coppia codice, nome) nella tabella
+# sostituire il nome con il codice
+# eliminare il campo "utente coinvolto"
+
+# salvare il file di log anonimizzato
+
+# salvare la tabella (codice, nome)
+
